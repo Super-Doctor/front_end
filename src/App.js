996ef485f02cea3c346eb7cada5e5039fe3fb595
mamoun kamal alshishani <mamoun.alshishani@yahoo.com>
2021-10-16 13:39:23 +0300

    fixingConflects

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import React from "react";
 import { BrowserRouter, Switch, Route} from "react-router-dom";
 import Department from './components/Departement/Department'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Footer from './components/footer/footer'
-import Header from './components/footer/footer'
-// import Home from './components/home'
+
+import Home from './components/Home'
 
 
 
@@ -13,22 +12,9 @@ function App() {
   
   return (
     <>
-    {/* <Header/> */}
-        {/* <BrowserRouter>
-          <Switch>
-            <Route exact path="/Home">
-          
-            </Route>
-        
-         
-         
-          </Switch>
-    
-        </BrowserRouter>
- */}
- <Header/> 
- <Department/>
- <Footer/>
+
+<Home/>
+
     </>
   );
 }
