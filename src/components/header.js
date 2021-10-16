642c0ff70e5bbffbf93e1e1055a3c9e7c93442ef
Ibrahim-Khdairat <ibrahimkuderat@gmail.com>
2021-10-16 14:05:14 +0300

    solving merge issues

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
-import React,{useContext} from "react";
+import React, { useContext } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Navbar, Nav, Container,Button } from "react-bootstrap";
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./home";
 import Footer from "./footer";
@@ -10,49 +10,50 @@ import { When } from 'react-if';
 
 
 
-function Header () {
-  const Login =  useContext(LoginContext)
-    return (
-      <div>
-        <Router>
-          <Navbar>
-            <Container>
+function Header() {
+  const Login = useContext(LoginContext)
+  return (
+    <div>
+      <Router>
+        <Navbar>
+          <Container>
             <Navbar.Brand href="/">
-                <div className="logo">
-                  <Link to="/">
-                    <img
-                      src='https://images.vexels.com/media/users/3/216150/isolated/lists/3bba4f96c0e0e2b6a38659dd09f5dcaf-doctor-hero-with-cape-character.png'
-                      style={{ height: "60px", objectFit: "cover" }}
-                    />
-                  </Link>
-                </div>
-              </Navbar.Brand>
-              <Navbar.Brand href="/">Super Doctor</Navbar.Brand>
-              <Nav className="me-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/departments">Departments</Nav.Link>
-                <Nav.Link href="/aboutus">About Us</Nav.Link>
-               
-              </Nav>
-              
-            </Container>
+              <div className="logo">
+                <Link to="/">
+                  <img
+                    src='https://images.vexels.com/media/users/3/216150/isolated/lists/3bba4f96c0e0e2b6a38659dd09f5dcaf-doctor-hero-with-cape-character.png'
+                    style={{ height: "60px", objectFit: "cover" }}
+                  />
+                </Link>
+              </div>
+            </Navbar.Brand>
+            <Navbar.Brand href="/">Super Doctor</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/departments">Departments</Nav.Link>
+              <Nav.Link href="/aboutus">About Us</Nav.Link>
 
-            <When condition={!Login.loggedIn}>
-            <Button onClick = {Login.toggleLogInState}>Login</Button>
+            </Nav>
+
+          </Container>
+
+          <When condition={!Login.loggedIn}>
+            <Button onClick={Login.toggleLogInState}>Login</Button>
           </When>
 
           <When condition={Login.loggedIn}>
-            
-            <Button onClick = {Login.logout}>Logout</Button>
+
+         
+            <Button onClick={Login.logout}>Logout</Button>
           </When>
 
 
-          </Navbar>
-        
-        </Router>
-      </div>
-    );
-  }
+        </Navbar>
+
+      </Router>
+    </div>
+  );
+}
 
 
 export default Header;
