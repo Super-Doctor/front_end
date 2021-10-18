6945765373b6a547273cd8d8dfd0809e686b6e61
Maram Abu Murad <maramabumurad97@gmail.com>
2021-10-18 18:26:38 +0300

    editting patients list

--- src/components/Patients/PatientsList.js
+++ src/components/Patients/PatientsList.js
@@ -2,21 +2,32 @@ import React, { useState, useContext, useEffect } from 'react';
 import axios from 'axios';
 import { LoginContext } from '../../context/login';
 import RecordList from './RecordList';
-
+import { Button, Card, Elevation } from "@blueprintjs/core";
+import './patients.css'
 export default function PatientsList() {
     const API = "https://super-doctors.herokuapp.com/doctorPatients";
-
+    const APIForAllPatient = 'https://super-doctors.herokuapp.com/allpatients'
     const Login = useContext(LoginContext);
     const [patientId, setPatient] = useState('');
-
     const [recordsList, setRecords] = useState([]);
-
+    const [AllPatients, setAllPatients] = useState([]);
+    const [formatted, setFormatted] = useState([]);
     useEffect(async () => {
-        if(Login.user) {
+        if (Login.user) {
             let records = await axios.get(`${API}/${Login.user.user.id}`);
-            console.log("1- From Api---> ", records.data);
-    
+            //console.log("1- From Api---> ", records.data);
+            let Patients = await axios.get(APIForAllPatient)
             setRecords(records.data);
+            let data = Patients.data.map(user => {
+                return ({
+                    userName: user.user.userName,
+                    id: user.user.id
+                })
+            })
+            console.log('data ', data);
+            //setAllPatients(data)
+            setFormatted(data)
+            //console.log('patients list',Patients.data);
             console.log("2- recordsList---> ", records.data);
             return true;
         }
@@ -30,11 +41,10 @@ export default function PatientsList() {
         console.log(patientId);
     }
 
-  
+
 
     return (
         <>
-          
             {patientId != '' &&
                 <>
                     <RecordList users={recordsList} patientId={patientId} />
@@ -45,17 +55,33 @@ export default function PatientsList() {
                 patientId == '' &&
                 <div>
                     <h1 >Patients List</h1>
-                    {recordsList.map((record) => {
+                    {recordsList.map((record, idx) => {
                         return (
-                            <li onClick={() => showRecord(`${record.patientId}`)} key={record.id}>{`Medical Case : ${record.medicalCase}    `} <span>{`  CheckIn Date : ${record.checkInDate}`}</span></li>
+                           <Card interactive={true} elevation={Elevation.TWO} onClick={() => showRecord(`${record.patientId}`)} key={record.id} className='patientsCard' >
+                                    <h5> {formatted.map(user => {
+                                        if (user.id == record.patientId) {
+                                            return (<span>{` Patient Name : ${user.userName}`}</span>)
+                                        }
 
+                                    })}</h5>
+                                    <p>{`CheckIn Date : ${record.checkInDate}`}</p>
+                                    <p>{`Medical Case : ${record.medicalCase}`}</p>
+
+                                    <button>Submit</button>
+                                </Card>
                         )
                     })}
 
 
                 </div>
             }
+
+
+
+
         </>
 
     )
 }
+
+
