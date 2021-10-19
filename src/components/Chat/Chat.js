b3300517d61885aa41df9e003acef8627b199ade
Ibrahim-Khdairat <ibrahimkuderat@gmail.com>
2021-10-19 11:22:25 +0300

    start chat

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,12 +1,55 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect , useState } from 'react'
 import { LoginContext } from '../../context/login'
-
-export default function Chat() {
+import { Form, Button } from 'react-bootstrap';
+import useForm from '../../hooks/form';
+import { handleChange, handleSubmit } from '../../hooks/form';
+// const messageBody = document.getElementById('message')
+export default function Chat(props) {
     const Login = useContext(LoginContext);
-    
+    const { handleChange, handleSubmit } = useForm(sendMessage);
+    const[messages , setMessages] = useState(null);
+
+    useEffect(() => {
+        props.socket.on("receive_message", data => {
+            console.log('receive_message ----> ', data);
+          if( Login.user.user.id == data.id) {
+                setMessages(data)
+            }
+        })
+
+    })
+
+    function sendMessage(message) {
+        console.log(message.message)
+
+        props.socket.emit('new_message', { message: message.message, id: props.id, userName: Login.user.user.userName })
+
+        // props.socket.on("receive_message", data => {
+        //     console.log('receive_message ----> ', data);
+        //     if( Login.user.user.id == data.id) {
+        //         setMessages(data)
+        //     }
+        // })
+    }
+
     return (
         <div>
-            
+            {messages && console.log('From Return',messages)}
+            <Form onSubmit={handleSubmit}>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+
+                    <Form.Control onChange={handleChange} type="text" name="message" placeholder="Enter your message" />
+
+                </Form.Group>
+
+
+
+                <Button variant="primary" type="submit">
+                    send
+                </Button>
+
+            </Form>
         </div>
     )
 }
--- src/components/Patients/RecordList.js
+++ src/components/Patients/RecordList.js
@@ -13,11 +13,15 @@ import Card from '@mui/material/Card';
 import React, { useState, useContext, useEffect } from 'react';
 import axios from 'axios';
 import { LoginContext } from '../../context/login';
+import { When } from "react-if";
+import Chat from "../Chat/Chat";
+import { io } from "socket.io-client";
 
 export default function RecordList(props) {
     const API = "https://super-doctors.herokuapp.com/patientRecords";
 
     const Login = useContext(LoginContext);
+    const [state, setChat] = useState(false);
 
 
 
@@ -29,65 +33,104 @@ export default function RecordList(props) {
         // const record=`https://super-doctors.herokuapp.com/medicalinfos/${Login.user.user.id}`;
         console.log(recordsLists.data);
 
-        setRecordsPatientList( recordsLists.data);
+        setRecordsPatientList(recordsLists.data);
         console.log("2- From Api---> ", recordsLists.data);
 
+    
     }, []);
 
-    console.log('Before Return =====>',recordsPatientList)
+    console.log('Before Return =====>', recordsPatientList)
+    let socket = io.connect('http://localhost:3001' , { transports : ['websocket'] });
+
+
+    const startChat = () => {
+        setChat(true);
+        console.log(io);
+        socket.emit("join-room" , 5,Login.user.user.userName);
+        
+        socket.emit("change_userName" ,{userName : Login.user.user.userName});
+    }
+
+    const endChat = () => {
+        setChat(false);
+        
+        socket.emit("disconnected" , Login.user.user.userName);
+    }
+
+
     return (
         <>
-            <h1>Patient Records</h1>
-            <Button>Start Chat</Button>
-        
 
-            { recordsPatientList.patientRecords &&  
-                <div>
-                    {
+            <When condition={!state}>
 
-                        recordsPatientList.patientRecords.map((record, index) => {
-
-                            //    if(props.patientId==recordsList.patientId){
-                            return (
-                                <Card style={{ display: "inline-block" }} sx={{ maxWidth: 345 }} className='cardieB'>
-                                    <CardActionArea>
-                                        <CardMedia
-                                            component="img"
-                                            height="300"
-                                            image='https://thumbs.dreamstime.com/b/boy-girl-doctors-cartoon-characters-profession-s-costume-doctor-vector-illustration-male-female-doctors-cartoon-147440124.jpg'
-                                            alt="eyes"
-                                        />
-                                        <CardContent>
-                                            <Typography gutterBottom variant="h5" component="div">
-                                                Name: { recordsPatientList.patientInfo.userName}
-                                            </Typography>
-                                            <Typography variant="body2" color="text.secondary">
-                                                Gender: { recordsPatientList.patientInfo.gender}
-                                            </Typography>
-                                            <Typography variant="body2" color="text.secondary">
-                                                Doctor Report: {record.doctorReport}
-                                            </Typography>
-                                            <Typography variant="body2" color="text.secondary">
-                                                Medical Case: {record.medicalCase}
-                                            </Typography>
-                                        </CardContent>
-                                    </CardActionArea>
-                             
-                                </Card>
+                <h1>Patient Records</h1>
+                <Button onClick={startChat}>Start Chat</Button>
 
 
-                            )
+                {recordsPatientList.patientRecords &&
+                    <div>
+                        {
+
+                            recordsPatientList.patientRecords.map((record, index) => {
+
+                                //    if(props.patientId==recordsList.patientId){
+                                return (
+                                    <Card style={{ display: "inline-block" }} sx={{ maxWidth: 345 }} className='cardieB'>
+                                        <CardActionArea>
+                                            <CardMedia
+                                                component="img"
+                                                height="300"
+                                                image='https://thumbs.dreamstime.com/b/boy-girl-doctors-cartoon-characters-profession-s-costume-doctor-vector-illustration-male-female-doctors-cartoon-147440124.jpg'
+                                                alt="eyes"
+                                            />
+                                            <CardContent>
+                                                <Typography gutterBottom variant="h5" component="div">
+                                                    Name: {recordsPatientList.patientInfo.userName}
+                                                </Typography>
+                                                <Typography variant="body2" color="text.secondary">
+                                                    Gender: {recordsPatientList.patientInfo.gender}
+                                                </Typography>
+                                                <Typography variant="body2" color="text.secondary">
+                                                    Doctor Report: {record.doctorReport}
+                                                </Typography>
+                                                <Typography variant="body2" color="text.secondary">
+                                                    Medical Case: {record.medicalCase}
+                                                </Typography>
+                                            </CardContent>
+                                        </CardActionArea>
+
+                                    </Card>
 
 
-                            //}
-                        })
-                    }
+                                )
+
+
+                                //}
+                            })
+                        }
 
 
 
 
-                </div>
-            }
+                    </div>
+                }
+            </When>
+            <When condition={state}>
+                <When condition={Login.user.user.roleId == 2 && state}>
+                    <h1>Doctor View</h1>
+                    <Button onClick={endChat}> Exit Chat </Button>
+                    <Chat socket={socket} id = {props.patientId}/>
+                </When>
+
+                <When condition={Login.user.user.roleId == 1 && state}>
+                    <h1>Patient View</h1>
+                    <Button onClick={endChat}> Exit Chat </Button>
+                    <Chat socket={socket} />
+                </When>
+            </When>
+
+
+
         </>
 
     )
